4ebe37d773e529eecb1a3a1968d632221d6acca2
Kelly <kellybytes.ca@gmail.com>
2025-12-02 12:48:12 -0700

    Add and style dashboard items with hard-coded data

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,9 +1,12 @@
 import Login from '@/components/Login';
+import SubscriptionForm from '@/components/SubscriptionForm';
 import SubscriptionsDisplay from '@/components/SubscriptionsDisplay';
 import SubscriptionSummary from '@/components/SubscriptionSummary';
 
 export default function DashboardPage() {
-  const isAuthenticated = false;
+  const isAuthenticated = true;
+  const isAddEntry = true;
+
   if (!isAuthenticated) {
     return <Login />;
   }
@@ -12,6 +15,7 @@ export default function DashboardPage() {
     <>
       <SubscriptionSummary />
       <SubscriptionsDisplay />
+      {isAddEntry && <SubscriptionForm />}
     </>
   );
 }
---
+++ components/SubscriptionForm.jsx
@@ -0,0 +1,109 @@
+'use client';
+
+export default function SubscriptionForm() {
+  return (
+    <section>
+      <h2>Add a new subscription</h2>
+      <form onSubmit={() => {}}>
+        <label>
+          <span>Subscription Name</span>
+          <input
+            type="text"
+            name="name"
+            placeholder="e.g. Netflix, Spotify, AWS Hosting"
+            required
+          />
+        </label>
+
+        <label>
+          <span>Category</span>
+          <select name="category">
+            {[
+              'Entertainment',
+              'Music',
+              'Software',
+              'Web Services',
+              'Health & Fitness',
+              'Other',
+            ].map((cat, catIndex) => {
+              return <option key={catIndex}>{cat}</option>;
+            })}
+          </select>
+        </label>
+
+        <label>
+          <span>Cost</span>
+          <input
+            type="number"
+            name="cost"
+            step="0.01"
+            placeholder="e.g. 12.00"
+            required
+          />
+        </label>
+
+        <label>
+          <span>Currency</span>
+          <select name="currency">
+            {['CAD', 'USD', 'EUR', 'GBP', 'Other'].map((cur, curIndex) => {
+              return <option key={curIndex}>{cur}</option>;
+            })}
+          </select>
+        </label>
+
+        <label>
+          <span>Billing Frequency</span>
+          <select name="billingFrequency">
+            {['Monthly', 'Yearly', 'Quarterly', 'One-time'].map(
+              (billing, billingIndex) => {
+                return <option key={billingIndex}>{billing}</option>;
+              }
+            )}
+          </select>
+        </label>
+
+        <label>
+          <span>Payment Method</span>
+          <select name="paymentMethod">
+            {[
+              'Credit Card',
+              'Debit Card',
+              'Paypal',
+              'Bank Transfer',
+              'Other',
+            ].map((payment, paymentIndex) => {
+              return <option key={paymentIndex}>{payment}</option>;
+            })}
+          </select>
+        </label>
+
+        <label>
+          <span>Subscription Start Date</span>
+          <input type="date" name="startDate" required />
+        </label>
+
+        <label>
+          <span>Status</span>
+          <select name="status">
+            {['Active', 'Paused', 'Cancelled'].map((status, statusIndex) => {
+              return <option key={statusIndex}>{status}</option>;
+            })}
+          </select>
+        </label>
+
+        <label className="fat-column">
+          <span>Notes</span>
+          <textarea
+            name="notes"
+            placeholder="e.g. Shared with family, includes cloud storage"
+          />
+        </label>
+
+        <div className="fat-column form-submit-btns">
+          <button>Cancel</button>
+          <button type="submit">Add Subscription</button>
+        </div>
+      </form>
+    </section>
+  );
+}
